8d7395e97264f056bf78fe17759b7c791ebac564
Eydan-gnt <eydan.gannot121@gmail.com>
2025-09-10 12:52:10 +0200

    premier models

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/ajouterProduit', function () {
+    
+    $produit = new Produit();
+    $produit->nomProduit = 'Produit 1';
+    $produit->descProduit = 'Produit 1 description';
+    $produit->prixProduit = 4.76;
+
+});
